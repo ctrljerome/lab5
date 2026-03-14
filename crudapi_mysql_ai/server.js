609b8615ccbc5a9872ca585f992e02f3d41672cb
ctrljerome <torresjeromed@gmail.com>
2026-03-14 19:20:56 +0800

    API health

--- crudapi_mysql_ai/server.js
+++ crudapi_mysql_ai/server.js
@@ -27,6 +27,12 @@ app.post("/mood", async (req, res) => {
   res.json({ message: "Mood saved successfully" });
 });
 
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "API running"
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
